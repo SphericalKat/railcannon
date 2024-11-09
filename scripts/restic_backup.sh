995df64cda5ffe9b46a429534ac413e25ea542a5
Sphericalkat <me@kat.bio>
2024-11-10 02:03:18 +0530

    feat: set up railcannon management using ansible
    Signed-off-by: Sphericalkat <me@kat.bio>

--- scripts/restic_backup.sh
+++ scripts/restic_backup.sh
@@ -5,26 +5,61 @@
 set -e -o pipefail
 
 BACKUP_TAG=systemd.timer
+
+# Retention policy
+RETENTION_DAYS=14
+RETENTION_WEEKS=16
+RETENTION_MONTHS=18
+RETENTION_YEARS=3
+
+# backup paths (array)
+BACKUP_PATHS=(
+    /mnt/backup-server/immich/backups/database/immich-db.sql
+    /mnt/backup-server/immich/library
+)
+
+EXCLUDE_PATHS=(
+    /mnt/backup-server/immich/library/thumbs
+    /mnt/backup-server/immich/library/encoded-video
+)
+
+# generate exclude options
+EXCLUDE_OPTIONS=""
+for EXCLUDE_PATH in "${EXCLUDE_PATHS[@]}" 
+do
+    EXCLUDE_OPTIONS+="--exclude $EXCLUDE_PATH "
+done
+
 source /etc/backup/restic_env
 
 # Remove locks in case other stale processes kept them in
 restic unlock &
 wait $!
 
-#Do the backup
 
-restic backup \
-       --verbose
-       --one-file-system \
-       --tag $BACKUP_TAG \
-       $BACKUP_PATH &
-
-wait $!
+#Do the backup for each path
+restic backup --verbose --tag $BACKUP_TAG /mnt/backup-server/immich/backups/database/immich-db.sql
+restic backup --verbose --tag $BACKUP_TAG /mnt/backup-server/immich/library --exclude /mnt/backup-server/immich/library/thumbs --exclude /mnt/backup-server/immich/library/encoded-video
 
 # Remove old Backups
+
+restic forget \
+       --verbose \
+       --tag $BACKUP_TAG \
+       --prune \
+       --keep-daily $RETENTION_DAYS \
+       --keep-weekly $RETENTION_WEEKS \
+       --keep-monthly $RETENTION_MONTHS \
+       --keep-yearly $RETENTION_YEARS &
+wait $!
 
 # Check if everything is fine
 restic check &
 wait $!
+
+unset RESTIC_PASSWORD
+unset RESTIC_REPOSITORY
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
 
 echo "Backup done!"
